65f9f65063a3d8cd4471d4bc6a322890d0cbdeeb
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-06-01 16:21:34 -0500

    feat: working on sessions and cookies exercises

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,20 +3,28 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/login.jsp").forward(request, response);
+        request.getRequestDispatcher("WEB-INF/login.jsp").forward(request, response);
     }
 
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         boolean validAttempt = username.equals("admin") && password.equals("password");
 
+        HttpSession session = request.getSession();
+        session.setAttribute("user", username);
+
+        if (session.getAttribute("user") != "admin")
         if (validAttempt) {
+
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
--- src/main/java/WelcomeServlet.java
+++ src/main/java/WelcomeServlet.java
@@ -4,12 +4,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
-@WebServlet(name = "AdsIndexServlet", urlPatterns = "/ads")
-public class AdsIndexServlet extends HttpServlet {
+@WebServlet(name = "WelcomeServlet", urlPatterns = "/")
+public class WelcomeServlet extends HttpServlet {
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("ads", DaoFactory.getAdsDao().all());
-        request.getRequestDispatcher("WEB-INF/ads/index.jsp").forward(request, response);
+        request.getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
     }
 }
